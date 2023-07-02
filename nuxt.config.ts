7571d55c70deb4acd8f345cb59561280fa1df0d2
Vedant Nandwana <vedantnn7.git@gmail.com>
2023-07-02 14:46:09 +0530

    use deno deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  nitro: {
+    preset: "deno"
+  },
   runtimeConfig: {
     defiApiKey: process.env.DEFI_API_KEY,
   },
